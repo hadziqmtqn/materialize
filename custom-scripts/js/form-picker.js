3badd08bda0328a8d323143ca2daace2c774b1b2
Hadziq <36812793+hadziqmtqn@users.noreply.github.com>
2025-08-27 07:13:33 +0700

    Remove altInput and altFormat from flatpickr

--- custom-scripts/js/form-picker.js
+++ custom-scripts/js/form-picker.js
@@ -47,8 +47,6 @@
     if (flatpickrDateTime.length > 0) {
         flatpickrDateTime.forEach((input) => {
             flatpickr(input, {
-                altInput: true,
-                altFormat: "F j, Y H:i",
                 locale: "id",
                 enableTime: true,
                 dateFormat: 'Y-m-d H:i',
